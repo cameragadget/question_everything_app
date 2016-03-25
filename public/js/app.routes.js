ada688bdc2fd8f1546a832aaaebbb8b9997fadf4
jonah sobol <jonah.sobol@gmail.com>
2016-03-25 12:27:37 -0700

    changes etc

---
+++ public/js/app.routes.js
@@ -0,0 +1,26 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('app')
+    .config(appRoutes);
+
+  appRoutes.$inject = ['$urlRouterProvider', '$stateProvider']
+
+  function appRoutes($urlRouterProvider, $stateProvider) {
+
+    $stateProvider
+      .state("welcome", {
+        url: "/",
+        templateUrl: "/js/app/layouts/welcome.html"
+      });
+
+      $urlRouterProvider.otherwise('/');
+
+  }
+
+
+
+
+
+})();
